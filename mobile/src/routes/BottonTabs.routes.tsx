0fe19f103593d7e8ebdc1dbf098b1c04dfa32652
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-07-15 21:31:39 -0300

    feat : created the screen and the navigator

--- mobile/src/routes/BottonTabs.routes.tsx
+++ mobile/src/routes/BottonTabs.routes.tsx
@@ -1,4 +1,14 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Welcome from "../screens/welcome";
+import Education from "../screens/education";
+import Projects from "../screens/projects";
 
-const tabs = createBottomTabNavigator()
+const Tab = createBottomTabNavigator()
 
+function MyTabs(){
+  <Tab.Navigator>
+    <Tab.Screen name="welcome" component={Welcome}  />
+    <Tab.Screen name="education" component={Education} />
+    <Tab.Screen name="project" component={Projects} />
+  </Tab.Navigator>
+}
---
+++ mobile/src/screens/projects.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Projects(){
+  return(
+    <View>
+      <Text>projects </Text>
+    </View>
+  )
+}
